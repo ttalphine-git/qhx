f12efa54f023bf2a12241ae5efa947141f4499fe
abhinand.c <abhinand.c@tahrtech.com>
2026-09-25 11:39:47 +0530

    test

--- halal-cms-backend/application-service/push-docker.sh
+++ halal-cms-backend/application-service/push-docker.sh
@@ -15,7 +15,6 @@ image_tag="latest"
 Dockerfile="Dockerfile"
 
 FULL_IMAGE="$container_registry/$contianer_image_owner/$image_name:$image_tag"
-
 echo "Logging into DigitalOcean Container Registry..."
 docker login "$container_registry"
 
